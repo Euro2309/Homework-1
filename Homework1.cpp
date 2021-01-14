8e2abb4d22e2e7a3f050113f8fae7de51bc9ef03
Euro2309 <77434379+Euro2309@users.noreply.github.com>
2021-01-14 16:29:24 +0700

    Add files via upload

---
+++ Homework1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << " 1st Term of 2020        " << "6206021631045 Ratchanon Kaewkulsri" << endl;
+	cout << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject name                        |" << "Score(100) | Grade |" << endl;
+	cout << "| 060243107  | COMPUTER ARCHITECTURE               |" << "    57     |  'D+'  |" << endl;
+	cout << "| 060243108  | DATABASE SYSTEM                     |" << "    63     |  'C'  |" << endl;
+	cout << "| 060243109  | COMPUTER NETWORK AND COMMUNICATION  |" << "    67     |  'C+'  |" << endl;
+	cout << "| 080303608  | CITIZENSHIP AND SOCIAL RESPONATION  |" << "    81     |  'A'  |" << endl;
+	cout << "| 080303609  | HEALTHY LIFE			   |" << "    78     |  'B'  |" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "|    TOTAL   | Subject = 5                         |" << "Average = 2.6     |" << endl;
+	cout << "==============================================================" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "KMUTNB" << endl;
+
+	return(0);
+}
